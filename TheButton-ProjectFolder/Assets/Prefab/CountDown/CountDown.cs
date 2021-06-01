105ece1732aaa24978bd933695094c035add02f8
Willian Johan <johan_willian@hotmail.com>
2021-06-01 17:23:14 -0300

    CountDown

---
+++ TheButton-ProjectFolder/Assets/Prefab/CountDown/CountDown.cs
@@ -0,0 +1,59 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour
+{
+    [SerializeField] int TotalSeconds = 60;
+    [SerializeField] TMP_Text SecondsDisplay;
+
+    float ElapsedSeconds;
+    bool IsStarded = false;
+
+    public static event Action OnFinishedTime;
+
+    private void Start()
+    {
+        ButtonBehaviour.OnButtonDown += HandleButtonDown;
+        OnFinishedTime += HandleFinishedTime;
+        SecondsDisplay.text = "Seconds: " + TotalSeconds.ToString();
+    }
+
+    private void OnDestroy()
+    {
+        OnFinishedTime -= HandleFinishedTime;
+        ButtonBehaviour.OnButtonDown -= HandleButtonDown;
+    }
+
+    void Update()
+    {
+        if (!IsStarded)
+            return;
+
+        ElapsedSeconds += Time.deltaTime;
+
+        if (TotalSeconds < ElapsedSeconds)
+            OnFinishedTime?.Invoke();
+        else
+            SecondsDisplay.text = (TotalSeconds - ElapsedSeconds).ToString().Substring(0, 4) + "s";
+    }
+
+    void HandleButtonDown()
+    {
+        ButtonBehaviour.OnButtonDown -= HandleButtonDown;
+        StartCounter();
+    }
+
+    void HandleFinishedTime()
+    {
+        IsStarded = false;
+        OnFinishedTime -= HandleFinishedTime;
+        SecondsDisplay.text = "0";
+    }
+
+    void StartCounter()
+    {
+        ElapsedSeconds = 0;
+        IsStarded = true;
+    }
+}
